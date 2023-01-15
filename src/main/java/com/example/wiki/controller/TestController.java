995464361f5d6e83c65d624c84f562742876ef27
lihua <zm0781@126.com>
2023-01-15 23:01:53 +0800

    3-5 测试了/test/list接口，查询test表所有数据

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,15 +1,24 @@
 package com.example.wiki.controller;
 
+import com.example.wiki.domain.Test;
+import com.example.wiki.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
     @Value("${test.hello:TEST}")
     private String testHello;
+
+    @Autowired
+    private TestService testService;
 
     /**
      * GET, POST, PUT, DELETE
@@ -31,5 +40,10 @@ public class TestController {
     @PostMapping("/hello/post")
     public String helloPost(String name) {
         return "Hello World! Post，" + name;
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }
