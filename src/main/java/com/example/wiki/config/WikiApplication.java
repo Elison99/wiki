ac645918d733831c3f530770eb4da05e6a0dcdc2
lihua <zm0781@126.com>
2023-01-15 18:23:35 +0800

    2-6 测试hello word接口

--- src/main/java/com/example/wiki/config/WikiApplication.java
+++ src/main/java/com/example/wiki/config/WikiApplication.java
@@ -1,12 +1,14 @@
-package com.example.wiki;
+package com.example.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@ComponentScan({"com.example.wiki"})
 public class WikiApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
---
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.example.wiki.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    /**
+     * GET, POST, PUT, DELETE
+     *
+     * /user?id=1
+     * /user/1
+     * @return
+     */
+    // @PostMapping
+    // @PutMapping
+    // @DeleteMapping
+    // @RequestMapping(value = "/user/1", method = RequestMethod.GET)
+    // @RequestMapping(value = "/user/1", method = RequestMethod.DELETE)
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
