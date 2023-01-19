895a6110a33878c511249a49403812d6da868861
lihua <zm0781@126.com>
2023-01-19 18:43:23 +0800

    8-9 文档管理布局美化 表格紧凑 按钮变小 表单变大 页面打开时就初始化宫文本框，z-index变小

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -3,7 +3,7 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-row>
+      <a-row :gutter="24">
         <a-col :span="8">
           <p>
             <a-form layout="inline" :model="param">
@@ -29,14 +29,14 @@
               :data-source="level1"
               :loading="loading"
               :pagination="false"
-              @change="handleTableChange"
+              size="small"
           >
-            <template #cover="{ text: cover }">
-              <img v-if="cover" :src="cover" alt="avatar"/>
+            <template #name="{ text: record }">
+              {{record.sort}} {{text}}
             </template>
             <template v-slot:action="{ text, record}">
               <a-space size="small">
-                <a-button type="primary" @click="edit(record)">
+                <a-button type="primary" @click="edit(record)" size="small">
                   编辑
                 </a-button>
                 <a-popconfirm
@@ -46,7 +46,7 @@
                     @confirm="handleDelete(record.id)"
                     @cancel="cancel"
                 >
-                  <a-button type="danger">
+                  <a-button type="danger" size="small">
                     删除
                   </a-button>
                 </a-popconfirm>
@@ -55,11 +55,20 @@
           </a-table>
         </a-col>
         <a-col :span="16">
-          <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-            <a-form-item label="名称">
-              <a-input v-model:value="doc.name"/>
+          <p>
+            <a-form layout="inline" :model="param">
+              <a-form-item>
+                <a-button type="primary" @click="handleSave()">
+                  保存
+                </a-button>
+              </a-form-item>
+            </a-form>
+          </p>
+          <a-form :model="doc" layout="vertical">
+            <a-form-item>
+              <a-input v-model:value="doc.name" placeholder="名称"/>
             </a-form-item>
-            <a-form-item label="父文档">
+            <a-form-item>
               <a-tree-select
                   v-model:value="doc.parent"
                   style="width: 100%"
@@ -71,7 +80,7 @@
               >
               </a-tree-select>
             </a-form-item>
-            <a-form-item label="顺序">
+            <a-form-item>
               <a-input v-model:value="doc.sort" />
             </a-form-item>
             <a-form-item label="内容">
@@ -126,18 +135,6 @@ export default defineComponent({
         slots: {customRender: 'name'}
       },
       {
-        title: '父文档',
-        key: 'parent',
-        dataIndex: 'parent',
-        slots: {customRender: 'parent'}
-      },
-      {
-        title: '顺序',
-        // key: 'doc2Id',
-        dataIndex: 'sort',
-        slots: {customRender: 'sort'}
-      },
-      {
         title: 'Action',
         key: 'action',
         slots: {customRender: 'action'}
@@ -182,7 +179,9 @@ export default defineComponent({
     const modalVisible = ref(false)
     const modalLoading = ref(false)
     const editor = new E('#content');
-    const handleModalOk = () => {
+    editor.config.zIndex = 0;
+
+    const handleSave = () => {
       modalLoading.value = true;
 
       axios.post("/doc/save",doc.value).then((response)=>{
@@ -329,7 +328,7 @@ export default defineComponent({
       edit,
       modalVisible,
       modalLoading,
-      handleModalOk,
+      handleSave,
 
       doc,
 
