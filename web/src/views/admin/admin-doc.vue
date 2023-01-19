bd8467bd7cfb4ff121aabeb7f90c96a3eacbe1de
lihua <zm0781@126.com>
2023-01-19 16:44:03 +0800

    8-6 删除某个文档时，增加二次确认 修复BUG:清空数组，否则次删除时，数组会一直增加

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -85,11 +85,12 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, ref} from 'vue';
+import {createVNode, defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
-import {message} from "ant-design-vue";
+import {message, Modal} from "ant-design-vue";
 import {Tool} from "@/util/tool";
 import {useRoute} from "vue-router";
+import { ExclamationCircleOutlined } from '@ant-design/icons-vue';
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -221,7 +222,8 @@ export default defineComponent({
     /**
      * 查找整根树枝
      */
-    const ids:Array<string> = [];
+    const deleteIds: Array<string> = [];
+    const deleteNames: Array<string> = [];
     const getDeleteIds = (treeSelectData: any, id: any) => {
       // console.log(treeSelectData, id);
       // 遍历数组，即遍历某一层节点
@@ -232,7 +234,8 @@ export default defineComponent({
           console.log("disabled", node);
           // 将目标节点设置为disabled
           // node.disabled = true;
-          ids.push(id);
+          deleteIds.push(id);
+          deleteNames.push(node.name);
 
           // 遍历所有子节点
           const children = node.children;
@@ -280,13 +283,23 @@ export default defineComponent({
 
     //删除
     const handleDelete = (id:any) => {
+      // 清空数组，否则多次删除时，数组会一直增加
+      deleteIds.length = 0;
+      deleteNames.length = 0;
       getDeleteIds(level1.value,id)
-      axios.delete("/doc/delete/"+ids.join(",")).then((response)=>{
-        const data = response.data;
-        if (data.success){
+      Modal.confirm({
+        title: '重要提醒',
+        icon: createVNode(ExclamationCircleOutlined),
+        content: '将删除：【' + deleteNames.join("，") + "】删除后不可恢复，确认删除？",
+        onOk(){
+          axios.delete("/doc/delete/"+deleteIds.join(",")).then((response)=>{
+            const data = response.data;
+            if (data.success){
 
-          //重新加载列表
-          handleQuery();
+              //重新加载列表
+              handleQuery();
+            }
+          })
         }
       })
     };
